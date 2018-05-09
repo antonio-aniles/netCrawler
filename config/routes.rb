a7b49f13feb77213824f59cdb87256a1ac757403
argogo <anilesantonio@gmail.com>
2018-05-08 23:31:48 -0700

    Add up/down vote method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links
+  resources :links do
+    member do
+      puts "like",    to: "links#peck"
+      puts "dislike", to: "links#pass"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "links#index"
 end
