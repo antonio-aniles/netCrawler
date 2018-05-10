4787d7023520a98647702a56978a3728937941a3
argogo <anilesantonio@gmail.com>
2018-05-09 23:50:16 -0700

    Correct member block

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :links do
     member do
-      puts "like",    to: "links#peck"
-      puts "dislike", to: "links#pass"
+      put "like",    to: "links#upvote"
+      put "dislike", to: "links#downvote"
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
